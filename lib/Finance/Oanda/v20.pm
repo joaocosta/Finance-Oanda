d543ecbb4aeb8fc814893cae18929a4300254f70
João Costa <joaocosta@zonalivre.org>
2016-09-07 18:22:39 +0100

    first commit

---
+++ lib/Finance/Oanda/v20.pm
@@ -0,0 +1,79 @@
+use strict;
+use warnings;
+package Finance::Oanda::v20;
+
+use Moose;
+use LWP::UserAgent;
+use JSON qw( decode_json );
+
+has 'account_id' => (
+    is  => 'rw',
+    isa => 'Str',
+    required => 1,
+);
+
+has 'auth_token' => (
+    is  => 'ro',
+    isa => 'Str',
+    required => 1,
+);
+
+has '_ua' => (
+    is => 'ro',
+    default => sub { LWP::UserAgent->new() },
+);
+
+has '_api_endpoint' => (
+    is => 'ro',
+    isa => 'URI',
+    default => sub { URI->new("https://api-fxtrade.oanda.com/v3/") },
+);
+
+
+sub _send_request {
+    my $self = shift;
+
+    my $req = HTTP::Request->new( @_ );
+    $req->header( 'Authorization' => 'Bearer ' . $self->auth_token );
+
+    return $self->_ua->request($req);
+}
+
+sub BUILD {
+}
+
+sub getAsk {
+    my ($self, $symbol) = @_;
+
+    my $url = $self->_api_endpoint->as_string() . "/accounts/" . $self->account_id . "/pricing?instruments=" . $symbol;
+    my $response = $self->_send_request(GET => $url);
+    my $response_object = decode_json( $response->decoded_content);
+
+    return $response_object->{prices}->[0]->{closeoutAsk};
+
+}
+
+sub getBid {
+    my ($self, $symbol) = @_;
+
+}
+
+sub openMarket {
+    my ($self, $direction, $amount) = @_;
+
+}
+
+sub closeMarket {
+    my ($self, $tradeID, $amount) = @_;
+}
+
+sub getTrades {
+}
+
+sub getBalance {
+}
+
+sub getMinTradeSize {
+}
+
+1;
